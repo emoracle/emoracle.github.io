f5333ea74e458fb9c720cf99a788864497f9d57a
Edwin van Meerendonk <vanmeerendonk@gmail.com>
2017-03-02 18:50:20 +0100

    return a cached version when things go south

--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,7 +1,37 @@
+var version = 1;
+var cacheName = "stale-" + version;
+
 self.addEventListener('install', function (e) {
-  console.log('Install event in service-worker.js: ', e);
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', function (e) {
-  console.log('Activate event in service-worker.js: ', e);
+  if (self.clients && clients.claim) {
+    clients.claim();
+  }
+});
+
+self.addEventListener('fetch', function (event) {
+  // fetch te respons from the network
+  event.respondWith(
+    fetch(event.request).then(function (response) {
+      caches.open(cacheName).then(function (cache) {
+        if (response.status >= 500) {
+          // when we receive an error we fetch a stale version from the cache
+          cache.match(event.request).
+          then(function (response) {
+            // We can not find the version in the cache
+            // So we return the respons from the network
+            return response;
+          }).catch (function () {
+            return response;
+          });
+        } else {
+          // Respons was < 500, so 200. We clone the respons in the cache
+          // and return the respons
+          cache.put(event.request.response.clone());
+          return response;
+        }
+      });
+    }));
 });
